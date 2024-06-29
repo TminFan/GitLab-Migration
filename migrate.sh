44a881fd9a6874e9017e1e81365e1e3fc562b387
t.fan <tianmin.fan@outlook.com>
2024-06-29 14:04:14 +1200

    Add check for existing repo

--- migrate.sh
+++ migrate.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -e
 
 github_repo_name=$1
-gitlab_repo_name=$2
-gitlab_user_name=$3
+github_user_name=$2
+gitlab_repo_name=$3
+gitlab_user_name=$4
 
 if [[ -n $GH_TOKEN && -n $GITLAB_ACCESS_TOKEN ]]; then
-    gh repo create "$github_repo_name" --private --source .
+    git ls-remote --heads --exit-codehttps://github.com/"$github_user_name"/"$github_repo_name"/git >/dev/null
+    echo "Does the GitHub repo $github_repo_name exist already? exit code is $?"
+    if [ "$?" > "0" ]
+    then
+        gh repo create "$github_repo_name" --private --source .
+    fi
     git clone -bare https://"$gitlab_user_name":"$GITLAB_ACCESS_TOKEN"@"$gitlab_repo_name"
-    git push -mirror "$github_repo_name"
+    git push -mirror https://github.com/"$github_user_name"/"$github_repo_name"/git
 else
     echo "EROOR: Tokens are not set in the environment variables."
     exit 1
