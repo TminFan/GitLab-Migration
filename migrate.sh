3321d33c8e2a1e8118c90159271ff80e91f8ec6c
t.fan <tianmin.fan@outlook.com>
2024-06-29 12:22:34 +1200

    Changed env token names

--- migrate.sh
+++ migrate.sh
@@ -4,9 +4,9 @@ github_repo_name=$1
 gitlab_repo_name=$2
 gitlab_user_name=$3
 
-if [[ -n $GITHUB_PERSONAL_TOKEN && -n $GITLAB_PERSONAL_TOKEN ]]; then
+if [[ -n $GH_TOKEN && -n $GITLAB_ACCESS_TOKEN ]]; then
     gh repo create "$github_repo_name" --private --source .
-    git clone -bare https://"$gitlab_user_name":"$GITLAB_TOKEN"@"$gitlab_repo_name"
+    git clone -bare https://"$gitlab_user_name":"$GITLAB_ACCESS_TOKEN"@"$gitlab_repo_name"
     git push -mirror "$github_repo_name"
 else
     echo "EROOR: Tokens are not set in the environment variables."
