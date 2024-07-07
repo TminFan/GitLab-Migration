3399dc98f8c29e68e0aaf36bdac73c732d0898ea
t.fan <tianmin.fan@outlook.com>
2024-07-08 10:00:15 +1200

    Changed to use credential file

--- pipeline-migration.sh
+++ pipeline-migration.sh
@@ -5,8 +5,8 @@ github_user_name=$2
 github_repo_name=$3
 gitlab_user_name=$4
 gitlab_repo_name=$5
-github_base_url="https://github.com/$github_user_name/"
-gitlab_base_url="https://gitlab.ecs.vuw.ac.nz"
+# github_base_url="https://github.com/$github_user_name/"
+# gitlab_base_url="https://gitlab.ecs.vuw.ac.nz"
 
 echo $audit
 
@@ -17,7 +17,8 @@ gh actions-importer -h
 
 # configure credentials
 if [[ -n $GH_TOKEN && -n $GITLAB_TOKEN ]]; then
-    echo -e "GitLab\n$GH_TOKEN\n$github_base_url\n$GITLAB_TOKEN\n$gitlab_base_url" | gh actions-importer configure
+    create-gh-cli-credentials-file.sh $github_user_name $gitlab_user_name
+    gh actions-importer --credentials-file importer_auth.yml
     gh actions-importer update
     echo "Finished config github actions importer and update"
 else
@@ -33,3 +34,5 @@ else
     echo "Start migrating pipelines"
     gh actions-importer migrate gitlab --target-url "https://$github_user_name:$GH_TOKEN@github.com/$github_user_name/$github_repo_name" --output-dir tmp/migrate --namespace $gitlab_user_name --project $gitlab_repo_name
 fi
+
+rm importer_auth.yml
