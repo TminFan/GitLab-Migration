c4b00c9c5aea117376169dc198bfa24fa216f70a
t.fan <tianmin.fan@outlook.com>
2024-07-07 19:09:42 +1200

    Added debug msgs

--- pipeline-migration.sh
+++ pipeline-migration.sh
@@ -28,5 +28,6 @@ if [[ "$audit" == "true" ]]; then
     echo "Start pipeline migration audit"
     ./pipeline-migration-audit.sh $gitlab_repo_name
 else
+    echo "Start migrating pipelines"
     gh actions-importer migrate gitlab --target-url "https://github.com/$github_user_name/$github_repo_name" --output-dir tmp/migrate --namespace $gitlab_user_name --project $gitlab_repo_name
 fi
