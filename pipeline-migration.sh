b9242573828da1be88e1b7f44eabe3536ae01bf8
t.fan <tianmin.fan@outlook.com>
2024-07-08 17:22:29 +1200

    Fixed input arg

--- pipeline-migration.sh
+++ pipeline-migration.sh
@@ -21,7 +21,7 @@ if [[ -n $GH_TOKEN && -n $GITLAB_TOKEN ]]; then
     if [[ "$audit" == "true" ]]; then
         chmod +x ./pipeline-migration-audit.sh
         echo "Start pipeline migration audit"
-        ./pipeline-migration-audit.sh $gitlab_repo_name
+        ./pipeline-migration-audit.sh $gitlab_user_name
     else
         echo "Start migrating pipelines"
         gh actions-importer migrate gitlab --gitlab-instance-url $gitlab_base_url --gitlab-access-token $GITLAB_TOKEN --github-instance-url $github_base_url --github-access-token $GH_TOKEN --target-url https://github.com/$github_user_name/$github_repo_name --output-dir tmp/migrate --namespace $gitlab_user_name --project $gitlab_repo_name
