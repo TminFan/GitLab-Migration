b5765445eb4bf6a8020091e0a009718b5fae8f69
t.fan <tianmin.fan@outlook.com>
2024-07-08 15:53:45 +1200

    test

--- pipeline-migration-audit.sh
+++ pipeline-migration-audit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 gitlab_namespace=$1
-# curr_repo_name=${{ github.repository }}
-curr_repo_name="TminFan/ecs_gitlab_migration"
+github_base_url="https://github.com"
+gitlab_base_url="https://gitlab.ecs.vuw.ac.nz"
 
-gh actions-importer audit gitlab --output-dir tmp/audit --namespace $gitlab_namespace
+gh actions-importer audit gitlab --gitlab-instance-url $gitlab_base_url --gitlab--access-token $GITLAB_TOKEN --github-instance-url $github_base_url --github-access-token $GH_TOKEN --output-dir tmp/audit --namespace $gitlab_namespace
+
+ls tmp/audit/
 
 content=$(<tmp/audit/audit_summary.md)
 content="${content//'%'/'%25'}"
@@ -15,15 +17,21 @@ curl \
 -X POST \
 -H "Accept: application/vnd.github.v3+json" \
 -H "Authorization: token $GH_TOKEN" \
-"https://api.github.com/repos/$curr_repo_name/check-runs" \
--d '{
-  "name": "Markdown Summary Check",
-  "head_sha": '${{ github.sha }}',
-  "status": "completed",
-  "conclusion": "success",
-  "output": {
-    "title": "Check Run Report",
-    "summary": '$content',
-    "text": "More details here"
-  }
-}'
+"https://api.github.com/repos/${{ github.repository }}/check-runs" \
+-d "$(jq -n \
+      --arg name "Markdown Summary Check" \
+      --arg head_sha "${{ github.sha }}" \
+      --arg title "Check Run Report" \
+      --arg summary "$content" \
+      --arg text "More details here" \
+      '{
+        name: $name,
+        head_sha: $head_sha,
+        status: "completed",
+        conclusion: "success",
+        output: {
+          title: $title,
+          summary: $summary,
+          text: $text
+        }
+      }')"
--- pipeline-migration.sh
+++ pipeline-migration.sh
@@ -5,8 +5,8 @@ github_user_name=$2
 github_repo_name=$3
 gitlab_user_name=$4
 gitlab_repo_name=$5
-# github_base_url="https://github.com/$github_user_name/"
-# gitlab_base_url="https://gitlab.ecs.vuw.ac.nz"
+github_base_url="https://github.com"
+gitlab_base_url="https://gitlab.ecs.vuw.ac.nz"
 
 echo $audit
 
@@ -14,33 +14,19 @@ gh extension install github/gh-actions-importer
 
 # verify the actions importer extension is installed
 gh actions-importer version
+gh actions-importer update
 
 # configure credentials
 if [[ -n $GH_TOKEN && -n $GITLAB_TOKEN ]]; then
-    chmod +x ./create-gh-cli-credential-file.sh
-    ./create-gh-cli-credential-file.sh $github_user_name $gitlab_user_name
-    ls -la
-    gh actions-importer configure --credentials-file importer_auth.yml
-    # gh actions-importer update
-    if [ $? ]; then
-        echo "Finished config github actions importer and update"
-        exit 1
+    if [[ "$audit" == "true" ]]; then
+        chmod +x ./create-gh-cli-credential-file.sh ./pipeline-migration-audit.sh
+        echo "Start pipeline migration audit"
+        ./pipeline-migration-audit.sh $gitlab_repo_name
     else
-        echo "ERROR: error with actions-importer configure"
+        echo "Start migrating pipelines"
+        gh actions-importer migrate gitlab --gitlab-instance-url $gitlab_base_url --gitlab--access-token $GITLAB_TOKEN --github-instance-url $github_base_url --github-access-token $GH_TOKEN --target-url https://github.com/$github_user_name/$github_repo_name --output-dir tmp/migrate --namespace $gitlab_user_name --project $gitlab_repo_name
     fi
 else
     echo "ERROR: Tokens are not set in the environment variables."
     exit 1
 fi
-
-# audit 
-if [[ "$audit" == "true" ]]; then
-    chmod +x ./create-gh-cli-credential-file.sh ./pipeline-migration-audit.sh
-    echo "Start pipeline migration audit"
-    ./pipeline-migration-audit.sh $gitlab_repo_name
-else
-    echo "Start migrating pipelines"
-    gh actions-importer migrate gitlab --target-url "https://github.com/$github_user_name/$github_repo_name" --output-dir tmp/migrate --namespace $gitlab_user_name --project $gitlab_repo_name --condentials-file importer_auth.yml
-fi
-
-rm importer_auth.yml
