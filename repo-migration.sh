f35fb3a2424bb3be6bc5056596c7775be31dc76d
t.fan <tianmin.fan@outlook.com>
2024-07-09 17:29:15 +1200

    Fixed incorrect gitlab project name for cloned repo

--- repo-migration.sh
+++ repo-migration.sh
@@ -16,10 +16,10 @@ if [[ -n $GH_TOKEN && -n $GITLAB_TOKEN ]]; then
         gh repo create "$github_repo_name" --private --source .
     fi
     # Clone the GitLab repo and migrate to the GitHub repo
-    git clone --bare https://"$gitlab_user_name":"$GITLAB_TOKEN"@"$gitlab_repo_full_path.git"
-    ls
-    cd $gitlab_repo_name.git
-    git push --mirror https://$github_user_name:$GH_TOKEN@github.com/"$github_user_name"/"$github_repo_name".git
+    git clone --bare https://"$gitlab_user_name":"$GITLAB_TOKEN"@"$gitlab_repo_full_path".git
+    gitlab_project_name="${$gitlab_repo_full_path##*/}"
+    cd $gitlab_project_name.git
+    git push --mirror https://"$github_user_name":"$GH_TOKEN"@github.com/"$github_user_name"/"$github_repo_name".git
 else
     echo "EROOR: Tokens are not set in the environment variables."
     exit 1
