fb01623c9b48ff8893db3810a30db609a997822f
TminFan <68936286+TminFan@users.noreply.github.com>
2024-07-09 18:03:57 +1200

    Enhance workflow variable flexibility (#2)
    * Changed gitlab base instance url to user input arg and code for verifying github actions-importer installation
    
    ---------
    
    Co-authored-by: t.fan

--- pipeline-migration-audit.sh
+++ pipeline-migration-audit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 gitlab_namespace=$1
-github_user_name=$2
-github_repo_name=
 github_base_url="https://github.com"
-gitlab_base_url="https://gitlab.ecs.vuw.ac.nz"
+gitlab_base_url=$2
 
+# Audit project pipelines of the given GitLab group
 gh actions-importer audit gitlab --gitlab-instance-url $gitlab_base_url --gitlab-access-token $GITLAB_TOKEN --github-instance-url $github_base_url --github-access-token $GH_TOKEN --output-dir tmp/audit --namespace $gitlab_namespace
 
+# Output audit summary to workflow job summary
 if [[ -f tmp/audit/audit_summary.md ]]; then
   echo "Migration audit report is found."
   content=$(cat tmp/audit/audit_summary.md)
--- repo-migration.sh
+++ repo-migration.sh
@@ -3,20 +3,23 @@
 github_user_name=$1
 github_repo_name=$2
 gitlab_user_name=$3
-gitlab_repo_name=$4
+gitlab_repo_full_path=$4
 
 
 if [[ -n $GH_TOKEN && -n $GITLAB_TOKEN ]]; then
+    # Check if a GitHub repo with the given repo name has already existed
     git ls-remote --heads --exit-code https://$github_user_name:$GH_TOKEN@github.com/"$github_user_name"/"$github_repo_name".git >/dev/null
     echo "Does the GitHub repo $github_repo_name exist already? exit code is $?"
     if [ "$?" > "0" ]
     then
+        # Create a new GitHub repo if not existing yet
         gh repo create "$github_repo_name" --private --source .
     fi
-    git clone --bare https://"$gitlab_user_name":"$GITLAB_TOKEN"@"gitlab.ecs.vuw.ac.nz/$gitlab_user_name/$gitlab_repo_name.git"
-    ls
-    cd $gitlab_repo_name.git
-    git push --mirror https://$github_user_name:$GH_TOKEN@github.com/"$github_user_name"/"$github_repo_name".git
+    # Clone the GitLab repo and migrate to the GitHub repo
+    git clone --bare https://"$gitlab_user_name":"$GITLAB_TOKEN"@"$gitlab_repo_full_path".git
+    gitlab_project_name="${gitlab_repo_full_path##*/}"
+    cd $gitlab_project_name.git
+    git push --mirror https://"$github_user_name":"$GH_TOKEN"@github.com/"$github_user_name"/"$github_repo_name".git
 else
     echo "EROOR: Tokens are not set in the environment variables."
     exit 1
