ed65c1545804bbbaf161c1ab6f7d8e6c83881630
t.fan <tianmin.fan@outlook.com>
2024-08-29 19:25:43 +1200

    Added condition for delete artifact job and modify api request data processing file paths

--- parser_scripts/gitlab_group_project_info_parser.py
+++ parser_scripts/gitlab_group_project_info_parser.py
@@ -1,3 +1,4 @@
+#python
 import json
 import os
 
@@ -7,16 +8,14 @@ def get_all_projects(data):
         projects.append(project["name"])
     
     projects_dict = {"projects": projects}
-    parser_output_dir = "parser_outputs"
-    outfile_path = os.path.join(os.getcwd(), parser_output_dir, "parsed.json")
+    outfile_path = os.path.join(os.getcwd(), "parsed.json")
     with open(outfile_path, "w") as outfile:
         json.dump(projects_dict, outfile)
     
     return projects
 
 if __name__ == "__main__":
-    json_file_dir = "api_calls_info"
-    file_path = os.path.join(os.getcwd(), json_file_dir, "all_gitlab_group_projects.json")
+    file_path = os.path.join(os.getcwd(), "all_gitlab_group_projects.json")
 
     with open(file_path, "r") as f:
         data = json.load(f)
